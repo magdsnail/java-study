0da060619ff60be5e4f2be569ebf9dcd386a20d5
jason.wang <wangkjim@gmail.com>
2022-06-20 20:04:42 +0800

    ..

---
+++ 基础语法/src/com/magdsnail/oop/demo05/A.java
@@ -0,0 +1,8 @@
+package com.magdsnail.oop.demo05;
+
+public class A extends B {
+    @Override
+    public void test() {
+        System.out.println("A=>test");
+    }
+}
---
+++ 基础语法/src/com/magdsnail/oop/demo05/B.java
@@ -0,0 +1,10 @@
+package com.magdsnail.oop.demo05;
+
+public class B {
+
+    public void test() {
+        System.out.println("B=>test");
+    }
+
+}
+
