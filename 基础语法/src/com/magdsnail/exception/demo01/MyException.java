84c1f59e3d0577279289adb757124775be22bfbd
wangkejing <wangkjim@gmail.com>
2023-01-12 10:19:22 +0800

    ..

--- 基础语法/src/com/magdsnail/exception/demo01/MyException.java
+++ 基础语法/src/com/magdsnail/exception/demo01/MyException.java
@@ -2,11 +2,15 @@ package com.magdsnail.exception.demo01;
 
 public class MyException extends Exception {
 
+    //传递参数>10
     private int detail;
 
     public MyException(int a) {
         this.detail = a;
     }
 
-    
+    @Override
+    public String toString() {
+        return "myException{" + detail + "}";
+    }
 }
--- 基础语法/src/com/magdsnail/exception/demo03/Test4.java
+++ 基础语法/src/com/magdsnail/exception/demo03/Test4.java
@@ -1,4 +1,17 @@
 package com.magdsnail.exception.demo03;
 
+import com.magdsnail.exception.demo01.MyException;
+
 public class Test4 {
+    static void test(int a) throws MyException {
+        System.out.println("传递参数为" + a);
+    }
+
+    public static void main(String[] args) {
+        try {
+            test(1);
+        } catch (MyException e) {
+            System.out.println("myexception" + e);
+        }
+    }
 }
